50f45136bc265d538ad87bb199b5e7fef109b29d
Alex Pavelko <oo.pavelko@gmail.com>
2022-11-20 21:21:10 +0100

    improve example

--- tricks/squares.py
+++ tricks/squares.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python
 
+squares = []
+
+for i in range(10):
+    squares.append(i**2)
+
+print(squares)
+# [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+
 print([i ** 2 for i in range(10)])
 # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
